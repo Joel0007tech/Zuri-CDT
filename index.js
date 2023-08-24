ed44d6b96dce25e25e1071d30ea9d90d616e6790
Joel <ajiolajoel@gmail.com>
2023-08-24 17:46:49 +0100

    update to zuri cdt

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ function toggleDropdown() {
     var dropdownContent = document.getElementById("dropdown-content");
     dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
   }
+  function toggleDrop() {
+    var dropdownContent = document.getElementById("dropdown-content_second");
+    dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+  }
+  function toggleDown() {
+    var dropdownContent = document.getElementById("dropdown-content_third");
+    dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+  }
  function toggle() {
     var mobileDropdown = document.getElementById("mobile-dropdown_content");
     mobileDropdown.style.display = mobileDropdown.style.display === "block" ? "none" : "block";

4c24398e035332a8f844397e8c4e5d7556e16b30
Joel <ajiolajoel@gmail.com>
2023-08-30 10:03:46 +0100

    update to zuri cdt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function toggleDropdown() {
     var dropdownContent = document.getElementById("dropdown-content");
-    dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+    dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" :"block";
   }
   function toggleDrop() {
     var dropdownContent = document.getElementById("dropdown-content_second");
@@ -14,3 +14,15 @@ function toggleDropdown() {
     var mobileDropdown = document.getElementById("mobile-dropdown_content");
     mobileDropdown.style.display = mobileDropdown.style.display === "block" ? "none" : "block";
  }
+ function Leave() {
+  var dropdownContent = document.getElementById("dropdown-content");
+  dropdownContent.style.display = dropdownContent.style.display === "none" ? "block" :"none";
+}
+function leaveSecond() {
+  var dropdownContent = document.getElementById("dropdown-content_second");
+  dropdownContent.style.display = dropdownContent.style.display === "none" ? "block" : "none";
+}
+function leaveThird() {
+  var dropdownContent = document.getElementById("dropdown-content_third");
+  dropdownContent.style.display = dropdownContent.style.display === "none" ? "block" : "none";
+}
